db9e5953e7ca12e746b3be8fd7778133c6952e36
anderson.cvlh <anderson.cvlh@gmail.com>
2018-05-25 23:33:17 -0300

    componente pag nao encontrada

---
+++ src/app/pagina-nao-encontrada/pagina-nao-encontrada.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'oak-pagina-nao-encontrada',
+  templateUrl: './pagina-nao-encontrada.component.html'
+})
+export class PaginaNaoEncontradaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
